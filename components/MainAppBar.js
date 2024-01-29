26f6fa82b2dd4669468d0e3785b1284ebfeafd87
kismakk <119887923+kismakk@users.noreply.github.com>
2024-01-29 23:48:20 +0200

    Assignment done

---
+++ components/MainAppBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Appbar } from 'react-native-paper'
+
+
+export default function MainAppBar(props) {
+  return (
+    <Appbar.Header elevated={true} >
+      <Appbar.Content title={props.title} />
+    </Appbar.Header>
+  )
+}
